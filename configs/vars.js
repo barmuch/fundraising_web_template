d441e2453492906555a1c06f6681a847efb60cc2
dfsdanimaulana <danimaulana9f@gmail.com>
2023-10-19 23:10:35 +0700

    fix: remove unused env config jwt

--- configs/vars.js
+++ configs/vars.js
@@ -15,13 +15,6 @@ export default {
     cloudinary_UPLOAD_PRESET_ARTICLE: process.env.CLOUDINARY_UPLOAD_PRESET_ARTICLE,
     google_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
     google_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    jwt: {
-        secret: process.env.JWT_SECRET,
-        accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
-        refreshExpirationDays: process.env.JWT_REFRESH_EXPIRATION_DAYS,
-        resetPasswordExpirationMinutes: process.env.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
-        verifyEmailExpirationMinutes: process.env.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
-    },
     email: {
         smtp: {
             host: process.env.SMTP_HOST,
